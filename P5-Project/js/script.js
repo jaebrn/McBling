188ba218589634be2f2099c58839daeb43e62402
jaebrn <80350744+jaebrn@users.noreply.github.com>
2023-03-30 17:16:39 -0400

    janky start menu + scene management

--- P5-Project/js/script.js
+++ P5-Project/js/script.js
@@ -1,33 +1,60 @@
-/**
-Title of Project
-Author Name
+let sceneIndex = 0;
+// 0 = main menu
+// 1 = 3,2,1
+// 2 = gameplay
+// 3 = ending
+let startButton;
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
-"use strict";
-
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+}
+
+function draw() {
+    switch (sceneIndex) { //scene management
+        case 0:
+            mainMenu();
+            break;
+        case 1:
+            countdown();
+            break;
+        case 2:
+            game();
+            break;
+        case 3:
+            end();
+            break;
+    }
+}
+
+function mainMenu() {
+    //drawing title
+    textSize(120);
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text('MCBLING SPEED TEXTER 3000', width / 2, height / 4);
+
+    //drawing start button
+    startButton = createButton('Start');
+    startButton.size(200, 200)
+    startButton.position(width / 2, height / 1.5);
+    startButton.mousePressed(nextScene);
 
 }
 
+function countdown() {
+    background(0);
+}
 
-/**
-Description of draw()
-*/
-function draw() {
+function game() {
+    background(0);
+}
 
+function end() {
+    background(0);
+}
+
+function nextScene() {
+    sceneIndex++;
+    print(sceneIndex);
 }
