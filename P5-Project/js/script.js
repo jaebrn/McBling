7c6099381c3526aba050c51cc91166720781525d
jaebrn <80350744+jaebrn@users.noreply.github.com>
2023-04-09 17:37:54 -0400

    Code cleanup + lerp bg

--- P5-Project/js/script.js
+++ P5-Project/js/script.js
@@ -4,30 +4,26 @@ let sceneIndex = 0;
 // 2 = gameplay
 // 3 = ending
 
-var startButton;
-let buttonCount = 0;
+var startButton, startButton2; // single and multiplayer start buttons
+var playerCount = 0; // number of players
+let buttonCount = 0; // number of buttons
 
-let timerStart;
-let timer;
+let timerStart; // time (millis()) when timer begins
+let timer; //countdown timer
 
-let score = 0;
-let lives = 3;
-let difficulty = 1;
-let yThreshold;
-let numberCount = 3;
-let numbers = [];
+let score, score2 = 0; // P1 & P2 scores
+let lives = 3; // number of lives
 
-//Thomas variables
+let yThreshold; // Y co-ord where numbers should be input
+let numberCount = 3; // # of numbers
+let numbers, numbers2 = []; // arrays storing numbers for P1 & P2
+
+//Input variables
 var p1Input = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; //keyTyped inputs for player 1, numbers 0 through 9
-var p2Input = ['p' ,'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o']; //same but for player2, represents numbers 0 through 9
-let numbers2 = [];
-let score2 = 0;
-var playerCount = 0;
-var startButton2;
+var p2Input = ['p', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o']; //same but for player2, represents numbers 0 through 9
 
 //score multiplier
-var scoreMult1 = 1;
-var scoreMult2 = 1;
+var scoreMult1, scoreMult2 = 1; // score multipliers for P1 & P2
 var scoreMultInc = 0.1;
 var thresholdHeight = 100;
 var hitScore = 100; //base value for a hit is 100 * by the position difference between the letter and the hitbox
@@ -48,14 +44,24 @@ var songIndex = 0;
 var songTitle = "";
 var songPlaying = true; //turns to false when song is completed playing
 
-function preload(){
+//colors
+let bgStart = (190, 150, 30);
+let bgNew = (255, 50, 255);
+let amt = 0;
+
+//images
+var corners;
+
+function preload() {
+    //loads songs and images
     loadSongs();
+    corners = loadImage('assets/images/corners.png');
 }
 
-function loadSongs(){ //put all audio file loading in here
+function loadSongs() { //put all audio file loading in here
     soundFormats('mp3');
     songList.push(loadSound('song/Butterfly-Smile.dk.mp3'));
-    songNames.push("Buttefly - Smile.dk");
+    songNames.push("Butterfly - Smile.dk");
     songList.push(loadSound('song/Captain_Jack-Captain_Jack.mp3'));
     songNames.push("Captain Jack - Captain Jack");
     songList.push(loadSound('song/Dreamscape-009_Sound_System.mp3'));
@@ -68,13 +74,15 @@ function loadSongs(){ //put all audio file loading in here
 
 function setup() {
     createCanvas(1440, 1080);
-    background(0);
-    // /noLoop();
     yThreshold = height - 100;
+
+    bgStart = color(190, 150, 30);
+    bgNew = color(255, 50, 255);
 }
 
 function draw() {
-    background(0);
+    bg(); // draws background graphics
+
     switch (sceneIndex) { //scene management
         case 0:
             mainMenu();
@@ -89,8 +97,26 @@ function draw() {
             end();
             break;
     }
+}
 
-    // print(sceneIndex);
+function bg() {
+    // color changing gradient background 
+    background(lerpColor(bgStart, bgNew, amt));
+    amt += 0.003;
+    if (amt >= 1.5) {
+        amt = 0.0;
+        bgStart = bgNew;
+        bgNew = color(random(255), random(255), random(255));
+    }
+
+    rectMode(CENTER);
+    noStroke();
+    fill(255, 100);
+    rect(width / 2, height / 2 + 20, width / 1.13, height / 1.2, 40); // translucent white screen outline
+    fill(0);
+    rect(width / 2, height / 2 + 20, width / 1.2, height / 1.3, 20); // translucent white screen outline
+
+    image(corners, 0, 0, 1440, 1080); //draws corner cutouts
 }
 
 function mainMenu() {
@@ -110,7 +136,7 @@ function mainMenu() {
         buttonCount++;
     }
 
-    if(buttonCount < 2){
+    if (buttonCount < 2) {
         startButton2 = createButton('Start 2 player');
         startButton2.size(200, 100);
         startButton2.position(width / 4, height / 1.5);
@@ -118,58 +144,50 @@ function mainMenu() {
         buttonCount++;
     }
 
-    if(buttonCount > 1){
+    if (buttonCount > 1) {
         //startButton.remove();
         //startButton2.remove();
     }
-    
+
 }
 
-function drawSongTitles(){
-    songTitle = "";
-    songTitle = text("< " + songNames[songIndex] + " >", width/2, height/2);
+function drawSongTitles() {
+    songTitle = text("< " + songNames[songIndex] + " >", width / 2, height / 2); //displays song title as text
 }
 
 function countdown() {
-    background(0);
-    if (millis() <= timerStart + 1000) {
-        timer = 3;
-    } else if (millis() <= timerStart + 2000) {
-        timer = 2;
-    }
-    else if (millis() <= timerStart + 3000) {
-        timer = 1;
-    } else {
-        startGame();
-    }
+    timer = int(map(millis(), timerStart, timerStart + 3000, 4, 1)); // maps time to seconds (3, 2, 1...)
 
     textSize(240);
     fill(255);
     textAlign(CENTER, CENTER);
-    text(timer, width / 2, height / 2);
+    text(timer, width / 2, height / 2); // displays timer as text
+
+    if (timer <= 0) { // starts game when timer reaches 0
+        startGame();
+    }
 }
 
 function game() {
-    background(0);
-   // fill(255, 0, 0);
+    // fill(255, 0, 0);
     //rect(0, yThreshold, width, 100);
     rectMode(CENTER);
-    if(playerCount == 1){
-       // fill(0, 255, 0);
+    if (playerCount == 1) {
+        // fill(0, 255, 0);
         noFill();
         strokeWeight(5);
         stroke(0, 255, 0);
         square(width / 2, yThreshold - 5, thresholdHeight);
-    }else if(playerCount == 2){
+    } else if (playerCount == 2) {
         //p1
-       // fill(0, 255, 0);
+        // fill(0, 255, 0);
         noFill();
         strokeWeight(5);
         stroke(0, 255, 0);
         square(width / 4, yThreshold - 5, thresholdHeight);
         //p2
-       // fill(0, 0, 255);
-       stroke(0, 0, 255);
+        // fill(0, 0, 255);
+        stroke(0, 0, 255);
         square(width * 0.75, yThreshold - 5, thresholdHeight);
 
     }
@@ -178,33 +196,33 @@ function game() {
     noStroke();
     //spawn numbers
     NumberSpawnerTimer();
-   
-    
+
+
 
     for (i = 0; i < numbers.length; i++) {
         numbers[i].move();
         if (numbers[i].y > height) { //height was yThreshold, now using it as the "hit" area to hit numbers in
-          //  lives--; commented out for testing purposes heehee
+            //  lives--; commented out for testing purposes heehee
             scoreMult1 = 1;
             print('lives ' + lives);
             numbers.splice(i, 1);
         }
     }
 
-    if(playerCount == 2){ //manages movement of numbers for player 2
-        for(var j = 0; j < numbers2.length; j++){
+    if (playerCount == 2) { //manages movement of numbers for player 2
+        for (var j = 0; j < numbers2.length; j++) {
             numbers2[j].move();
-            if(numbers2[j].y > height){
+            if (numbers2[j].y > height) {
                 //there should be player 2 lives here but whatever
                 scoreMult2 = 1;
                 numbers2.splice(j, 1);
             }
         }
     }
-  
+
 
     printScore();
-   // printLives();
+    // printLives();
 
     if (lives <= 0) {
         sceneIndex = 3;
@@ -212,21 +230,20 @@ function game() {
 }
 
 function end() {
-    background(0);
     textSize(70);
-    if(playerCount == 1){
-      //  text("Game Over :(", width / 2, height / 2);
-      text("Score:" + score, width/2, height/2);
-    }else if(playerCount == 2){
-        text("Player 1 Score:" + score, width/4, height/4);
-        text("Player 2 Score:" + score2, width * 0.75, height/4);
-        if(score > score2){
-            text("Player 1 Wins!", width/2, height/2);
-        }else{
-            text("Player 2 Wins!", width/2, height/2);
+    if (playerCount == 1) {
+        //  text("Game Over :(", width / 2, height / 2);
+        text("Score:" + score, width / 2, height / 2);
+    } else if (playerCount == 2) {
+        text("Player 1 Score:" + score, width / 4, height / 4);
+        text("Player 2 Score:" + score2, width * 0.75, height / 4);
+        if (score > score2) {
+            text("Player 1 Wins!", width / 2, height / 2);
+        } else {
+            text("Player 2 Wins!", width / 2, height / 2);
         }
     }
-    
+
     /*
     startButton.show();
     startButton2.show();*/
@@ -242,7 +259,7 @@ function startCountdown() { //start button ID is used to tell how many players a
 }
 
 //also thomas
-function startCountdown2(){
+function startCountdown2() {
     playerCount = 2;
     timerStart = millis();
     sceneIndex = 1;
@@ -257,7 +274,7 @@ function startGame() {
     songList[songIndex].onended(endsong);
 }
 
-function endsong(){
+function endsong() {
     print("song has ended");
     songPlaying = false
 };
@@ -274,18 +291,18 @@ function printScore() {
     // text("Score: " + score, width - 100, 100);
     //p1
     textAlign(LEFT);
-   text("Score: " + score, 0, 100);
-   text("x" + scoreMult1, 0, 200);
+    text("Score: " + score, 0, 100);
+    text("x" + scoreMult1, 0, 200);
 
-   if(playerCount == 2){
-    //p2
-    textAlign(RIGHT);
-    text("Score: " + score2, width, 100);
-    text("x" + scoreMult2, width, 200);
-   }
-   
+    if (playerCount == 2) {
+        //p2
+        textAlign(RIGHT);
+        text("Score: " + score2, width, 100);
+        text("x" + scoreMult2, width, 200);
+    }
 
-   textAlign(CENTER);
+
+    textAlign(CENTER);
 }
 
 class Numbers {
@@ -300,89 +317,89 @@ class Numbers {
     move() {
         textSize(96);
         text(this.number, this.x, this.y);
-        if(this.playerID == 1){
+        if (this.playerID == 1) {
             this.y += this.speed * scoreMult1;
-        }else if(this.playerID == 2){
+        } else if (this.playerID == 2) {
             this.y += this.speed * scoreMult2;
         }
-        
+
     }
 }
 
 //Thomas made this
-function keyTyped(){ //automatically receives key inputs
+function keyTyped() { //automatically receives key inputs
     getInput(key);
 }
 
-function getInput(value){ //sorts out what to do with key input based on scene index
+function getInput(value) { //sorts out what to do with key input based on scene index
     print(value);
-    switch(sceneIndex){
+    switch (sceneIndex) {
         case 2: //game input
             gameInput(value);
-        break;
+            break;
         case 0: //main menu
-        menuInput(value);
-        break;
+            menuInput(value);
+            break;
         case 3:
-        endInput(value);
-        break;
+            endInput(value);
+            break;
     }
 }
 
-function menuInput(value){
-    switch(value){
+function menuInput(value) {
+    switch (value) {
         case "Enter":
             startCountdown(); //1 player start
-        break;
+            break;
 
         case ' ': //2 player start
             startCountdown2();
-        break;
+            break;
 
         case '6': //next song
         case '8':
         case 'i':
         case 'y':
             songIndex++;
-            if(songIndex > songList.length - 1 || songIndex > songNames.length - 1){
+            if (songIndex > songList.length - 1 || songIndex > songNames.length - 1) {
                 songIndex = 0;
             }
-        break;
+            break;
 
         case '4': //previous song
         case '2':
         case 'r':
         case 'w':
             songIndex--;
-            if(songIndex < 0){
+            if (songIndex < 0) {
                 songIndex = songList.length - 1;
             }
-        break;
+            break;
     }
 }
 
-function endInput(value){
-    switch(value){
+function endInput(value) {
+    switch (value) {
         case "Enter":
         case ' ':
             sceneIndex = 0;
             songPlaying = true;
-        break;
+            break;
     }
 }
 
-function gameInput(value){
+function gameInput(value) {
     var numberID; //which number is this key associated with
     var playerID; //which player is this key associated with
-    for(var i = 0; i < p1Input.length; i++){
-        if(p1Input[i] == value){
+    for (var i = 0; i < p1Input.length; i++) {
+        if (p1Input[i] == value) {
             numberID = i;
             playerID = 1;
         }
     }
 
-    for(var j = 0; j < p2Input.length; j++){
-        if(p2Input[j] == value){
+    for (var j = 0; j < p2Input.length; j++) {
+        if (p2Input[j] == value) {
             numberID = j;
             playerID = 2;
         }
@@ -390,47 +407,47 @@ function gameInput(value){
     print("received input " + numberID + "for player " + playerID);
     //check if an onscreen number matches the input
     //the copy pasting of stuff did a fucking number on the formatting fml
-    if(playerID == 1){
+    if (playerID == 1) {
         var received = false; //basically if this is true it will stop checking for a number onscreen, so if theres duplicate numbers on screen it should only destroy the closest one.
-    for(var l = 0; l < numbers.length; l++){
-        if(!received){
-            if(numberID == numbers[l].number){
-                if(numbers[l].y >= yThreshold - (thresholdHeight / 2)){
-                    //score++;
-                    scorePoints(1, numbers[l].y);
-                    numbers.splice(l, 1);
-                    received = true;
+        for (var l = 0; l < numbers.length; l++) {
+            if (!received) {
+                if (numberID == numbers[l].number) {
+                    if (numbers[l].y >= yThreshold - (thresholdHeight / 2)) {
+                        //score++;
+                        scorePoints(1, numbers[l].y);
+                        numbers.splice(l, 1);
+                        received = true;
+                    }
                 }
             }
         }
-    }
 
-    }else if(playerID == 2){
+    } else if (playerID == 2) {
         var received = false; //basically if this is true it will stop checking for a number onscreen, so if theres duplicate numbers on screen it should only destroy the closest one.
-        for(var l = 0; l < numbers2.length; l++){
-            if(!received){
-                if(numberID == numbers2[l].number){
-                    if(numbers2[l].y >= yThreshold - (thresholdHeight / 2)){
-                    //score++;
-                    scorePoints(2, numbers2[l].y);
-                    numbers2.splice(l, 1);
-                    received = true;
+        for (var l = 0; l < numbers2.length; l++) {
+            if (!received) {
+                if (numberID == numbers2[l].number) {
+                    if (numbers2[l].y >= yThreshold - (thresholdHeight / 2)) {
+                        //score++;
+                        scorePoints(2, numbers2[l].y);
+                        numbers2.splice(l, 1);
+                        received = true;
+                    }
                 }
             }
         }
-    }
 
     }
-    
+
 }
 
-function scorePoints(player, ypos){
+function scorePoints(player, ypos) {
     var reward = 0;
     var posbonus = 0;
-    if(player == 1){
-        if(ypos < yThreshold){
+    if (player == 1) {
+        if (ypos < yThreshold) {
             posbonus = ypos / yThreshold;
-        }else{
+        } else {
             posbonus = yThreshold / ypos;
         }
         scoreMult1 += scoreMultInc;
@@ -442,10 +459,10 @@ function scorePoints(player, ypos){
         print("posBonus is " + posbonus);
     }
 
-    if(player == 2){
-        if(ypos < yThreshold){
+    if (player == 2) {
+        if (ypos < yThreshold) {
             posbonus = ypos / yThreshold;
-        }else{
+        } else {
             posbonus = yThreshold / ypos;
         }
         scoreMult2 += scoreMultInc;
@@ -460,38 +477,38 @@ function scorePoints(player, ypos){
     print("player " + player + " gains " + reward + " points");
 }
 
-function NumberSpawnerTimer(){ //manages timers to spawn numbers for players
-    if(songPlaying == true){
-//p1
-timer1 += deltaTime;
-if(timer1 >= timerLimit1){
-    SpawnNumber(1);
-    timer1 = 0;
-    timerLimit1 = random(timerMIN, timerMAX);
-}
+function NumberSpawnerTimer() { //manages timers to spawn numbers for players
+    if (songPlaying == true) {
+        //p1
+        timer1 += deltaTime;
+        if (timer1 >= timerLimit1) {
+            SpawnNumber(1);
+            timer1 = 0;
+            timerLimit1 = random(timerMIN, timerMAX);
+        }
 
-//p2
-if(playerCount == 2){
-    timer2 += deltaTime;
-    if(timer2 >= timerLimit2){
-        SpawnNumber(2);
-        timer2 = 0;
-        timerLimit2 = random(timerMIN, timerMAX);
-    }
-}
-    }else{
+        //p2
+        if (playerCount == 2) {
+            timer2 += deltaTime;
+            if (timer2 >= timerLimit2) {
+                SpawnNumber(2);
+                timer2 = 0;
+                timerLimit2 = random(timerMIN, timerMAX);
+            }
+        }
+    } else {
         sceneIndex = 3;
     }
-    
+
 }
 
-function SpawnNumber(player){ //called by NumberSpawnerTimer when a timer runs out to ACTUALLY spawn a timer
-    if(playerCount == 1){
+function SpawnNumber(player) { //called by NumberSpawnerTimer when a timer runs out to ACTUALLY spawn a timer
+    if (playerCount == 1) {
         numbers.push(new Numbers(-500 * numbers.length, width / 2, 1));
-    }else if(playerCount == 2){
-        if(player == 1){
+    } else if (playerCount == 2) {
+        if (player == 1) {
             numbers.push(new Numbers(-500 * numbers.length, width / 4, 1)); // needs to be changed to be flexible
-        }else if(player == 2){
+        } else if (player == 2) {
             numbers2.push(new Numbers(-500 * numbers2.length, width * 0.75, 2));
         }
     }
